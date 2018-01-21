ec82529489319a9f8b59a686de05529e24c5ced5
Tim Potter <tpot@frungy.org>
2018-01-21 11:51:48 +1100

    Add whitespace and rearrange

--- setup.py
+++ setup.py
@@ -6,25 +6,34 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
+
     name = "yamljsonmodel",
     version = "0.0.1",
     author = "Tim Potter",
     author_email = "tpot@booleancandy.com.au",
-    description = ("Generate JSONModel subclasses from YAML files."),
     license = "Apache-2",
+    
+    description = ("Generate JSONModel subclasses from YAML files."),
+    long_description = read('README'),
+    
     keywords = "JSONModel YAML",
     url = "http://packages.python.org/yamljsonmodel",
-    install_requires = ['jinja2 >= 2.9', 'pyyaml'],
-    packages = ['yamljsonmodel', 'tests'],
-    scripts = ['scripts/yamljsonmodel'],
-    package_data = {
-        'yamljsonmodel': ['templates/*/*'],
-    },
-    long_description = read('README'),
+    
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Code Generators",
         "License :: OSI Approved :: Apache Software License",
     ],
+
+    install_requires = ['jinja2 >= 2.9', 'pyyaml'],
+    
+    packages = ['yamljsonmodel', 'tests'],
+    
+    scripts = ['scripts/yamljsonmodel'],
+    
+    package_data = {
+        'yamljsonmodel': ['templates/*/*'],
+    },
+    
 )
