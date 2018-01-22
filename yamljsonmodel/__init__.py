7b556e444646a7f83d4793bdadb0363bad854809
Tim Potter <tpot@frungy.org>
2018-01-22 21:25:37 +1100

    Move plugin loading and selection into yamljsonmodel module

---
+++ yamljsonmodel/__init__.py
@@ -0,0 +1,20 @@
+import pkg_resources
+
+# Load plugins using pkg_resources
+
+plugins = {
+    entry_point.name: entry_point.load()
+    for entry_point in pkg_resources.iter_entry_points('yamljsonmodel.plugins')
+}
+
+def process_yaml_document(yamldoc):
+
+    for obj in yamldoc:
+
+        kind = obj['kind']
+
+        if not plugins.has_key(kind):
+            raise Exception('No plugin named %s' % kind)
+
+        plugin = plugins[kind]
+        plugin.process_yaml_object(kind, obj)
